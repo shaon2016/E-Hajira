f8882dbb4e4ee054186636cff8a792c6ee80ce1c
Shaon <shaondeveloper@gmail.com>
2019-11-15 19:49:21 +0600

    File saved successfully

--- app/src/main/java/com/copotronic/stu/activities/AddNoticeActivity.kt
+++ app/src/main/java/com/copotronic/stu/activities/AddNoticeActivity.kt
@@ -1,19 +1,38 @@
 package com.copotronic.stu.activities
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import com.copotronic.stu.R
 import com.copotronic.stu.data.AppDb
+import com.copotronic.stu.helper.U
 import com.copotronic.stu.model.UserType
+import com.esafirm.imagepicker.features.ImagePicker
+import com.esafirm.imagepicker.model.Image
 import kotlinx.android.synthetic.main.activity_add_notice.*
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.channels.FileChannel
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class AddNoticeActivity : AppCompatActivity() {
     private lateinit var db: AppDb
     private var typeId = 0
+    /** Request code for gallery image selection for ad post*/
+    private val REQUEST_GALLERY_IMAGE = 231
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +43,22 @@ class AddNoticeActivity : AppCompatActivity() {
     }
 
     private fun initVar() {
-
+        db = AppDb.getInstance(this)!!
     }
 
     private fun initView() {
         setUserTypes()
+
+        btnAddImageNotice.setOnClickListener {
+            ImagePicker.create(this)
+                .toolbarFolderTitle(getString(R.string.folder)) // folder selection title
+                .toolbarImageTitle(getString(R.string.tap_to_select)) // image selection title
+                .toolbarArrowColor(Color.BLACK)
+                .limit(1)
+                .showCamera(true)
+                .toolbarArrowColor(ContextCompat.getColor(this, R.color.white))
+                .start(REQUEST_GALLERY_IMAGE)
+        }
     }
 
     private fun setUserTypes() {
@@ -60,5 +90,31 @@ class AddNoticeActivity : AppCompatActivity() {
             } else spinUserType.visibility = View.GONE
         })
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_GALLERY_IMAGE && resultCode == Activity.RESULT_OK) {
+            val image = ImagePicker.getFirstImageOrNull(data)
+
+            copyFileToDestination(image)
+
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun copyFileToDestination(image: Image) {
+        val calender = Calendar.getInstance()
+
+        val src = File(image.path)
+        val destination = File(
+            getExternalFilesDir(
+                Environment.DIRECTORY_PICTURES
+            ), "${calender.timeInMillis}${image.name}"
+        )
+
+        Thread {
+            U.copyOrMoveFile(src, destination, true)
+        }.start()
+    }
+
 
 }
--- app/src/main/java/com/copotronic/stu/helper/U.kt
+++ app/src/main/java/com/copotronic/stu/helper/U.kt
@@ -1,5 +1,10 @@
 package com.copotronic.stu.helper
 
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.channels.FileChannel
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -37,4 +42,25 @@ object U {
             val date = Date()
             return df.format(date)
         }
+
+    /**
+     * For src -> source folder and file
+     * destination -> destination path and new name
+     * */
+    @Throws(IOException::class)
+     fun copyOrMoveFile(srcFile: File, destination: File, isCopy: Boolean) {
+        var outChannel: FileChannel? = null
+        var inputChannel: FileChannel? = null
+        try {
+            outChannel = FileOutputStream(destination).channel
+            inputChannel = FileInputStream(srcFile).channel
+            inputChannel.transferTo(0, inputChannel.size(), outChannel)
+            inputChannel.close()
+            if (!isCopy)
+                srcFile.delete()
+        } finally {
+            inputChannel?.close()
+            outChannel?.close()
+        }
+    }
 }
